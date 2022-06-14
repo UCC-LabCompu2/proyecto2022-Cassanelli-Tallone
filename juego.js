3e25128ae9ab3292c090387b59d6206e5f4ccf7c
Tomas Cassanelli <2102092@ucc.edu.ar>
2022-06-14 14:58:59 -0300

    avances

--- juego.js
+++ juego.js
@@ -218,12 +218,23 @@ if (document.getElementsByClassName("botonesCI")[0]) {
   };
 }
 
-window.addEventListener("load", () => {
-  const colorItems = document.querySelectorAll(".color-item");
-  colorItems.forEach((item) => {
-    item.addEventListener("click", function () {
-      const idSelected = this.id;
-      document.body.className = idSelected;
-    });
-  });
-});
+//Se utiliza para que el campo de texto solo acepte letras
+function soloLetras(e) {
+  key = e.keyCode || e.which;
+  tecla = String.fromCharCode(key).toString();
+  letras = " áéíóúabcdefghijklmnñopqrstuvwxyzÁÉÍÓÚABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; //Se define todo el abecedario que se quiere que se muestre.
+  especiales = [8, 37, 39, 46, 6]; //Es la validación del KeyCodes, que teclas recibe el campo de texto.
+
+  tecla_especial = false;
+  for (var i in especiales) {
+    if (key == especiales[i]) {
+      tecla_especial = true;
+      break;
+    }
+  }
+
+  if (letras.indexOf(tecla) == -1 && !tecla_especial) {
+    alert("Tecla no aceptada");
+    return false;
+  }
+}
